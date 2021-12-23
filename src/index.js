92ddba82eec5d0051b811fdc17e818f8f3227828
Kelvin Etori <kelvin@email.arizona.edu>
2021-12-23 02:02:21 -0700

    finished menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import "./style/style.css";
-import "./img/berrylot.jpeg";
-
+import "./style/home.css";
+import "./style/menu.css";
 import { setUp } from "./modules/frontPage.js";
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const images = importAll(
+  require.context("./img/", false, /\.(png|jpe?g|svg)$/)
+);
 
 function setUpPage() {}
 
