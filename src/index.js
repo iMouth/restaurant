9ddfaa9c51e55d04b3ab5888d09989fd00d4c402
Kelvin Etori <kelvin@email.arizona.edu>
2021-12-23 21:03:51 -0700

    added event listeners

--- src/index.js
+++ src/index.js
@@ -11,6 +11,35 @@ const images = importAll(
   require.context("./img/", false, /\.(png|jpe?g|svg)$/)
 );
 
-function setUpPage() {}
+function remove() {
+  // TODO: Implement later
+}
+
+function displayMenu() {
+  let menu = document.getElementById("menu");
+  menu.style.display = "flex";
+}
+
+document.getElementById("menu-user").addEventListener("click", displayMenu);
+
+function displayHome() {
+  let home = document.getElementById("pic-slogan");
+  home.style.display = "flex";
+}
+
+document.getElementById("home-user").addEventListener("click", displayHome);
+
+function displayContact() {
+  let home = document.getElementById("contact");
+  home.style.display = "flex";
+}
+
+document
+  .getElementById("contact-user")
+  .addEventListener("click", displayContact);
+
+function setUpPage() {
+  displayHome();
+}
 
 setUpPage();
